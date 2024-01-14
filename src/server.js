4a3a945d65c51757749af6156b952eac3a162e67
programmermedical <programmermedical@gmail.com>
2024-01-15 06:24:03 +0700

    first commit version 2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 /* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -16,7 +16,12 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
